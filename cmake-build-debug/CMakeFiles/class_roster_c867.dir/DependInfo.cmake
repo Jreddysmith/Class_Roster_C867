5a1e08fafee846532b52cadba1f2f950ef0483cc
Jarred Smith <jreddysmith@gmail.com>
2020-01-09 14:36:36 -0600

    Firsttttttt

---
+++ cmake-build-debug/CMakeFiles/class_roster_c867.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Jarred/Documents/class_projects/class_roster_c867/main.cpp" "/Users/Jarred/Documents/class_projects/class_roster_c867/cmake-build-debug/CMakeFiles/class_roster_c867.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/class_roster_c867.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/class_roster_c867.dir/main.cpp.o"
+  "class_roster_c867"
+  "class_roster_c867.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/class_roster_c867.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
